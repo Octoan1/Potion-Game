b6976639eab7a6cb2e27c1994dbea94e3775059b
Octoan1 <okloepping@gmail.com>
2026-04-13 23:43:24 -0500

    fixed doors, now use ids to traverse

--- levels/level_manager.gd
+++ levels/level_manager.gd
@@ -7,6 +7,8 @@ class_name LevelManager
 
 @export var player: CharacterBody2D
 
+var next_door_id : String = ""
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	load_level(starting_level)
@@ -21,5 +23,16 @@ func load_level(path: String = starting_level) -> void:
 	var level: Node2D = load(path).instantiate()
 	self.add_child(level)
 	
-	var spawn_point: Marker2D = level.find_child("Door").spawn_point
-	player.global_position = spawn_point.global_position
+	place_player()
+
+
+func place_player() -> void:
+	if next_door_id == "":
+		return
+	
+	var doors: Array[Node] = get_tree().get_nodes_in_group("doors")
+	
+	for door in doors:
+		if door.door_id == next_door_id:
+			player.global_position = door.global_position
+			break
