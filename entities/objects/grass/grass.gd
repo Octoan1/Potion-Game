db4509e7d9250e753a6881d84ce7eb17cb3da2ee
Octoan1 <okloepping@gmail.com>
2026-04-21 14:15:10 -0500

    item pop and wavy grass

---
+++ entities/objects/grass/grass.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite2D
+
+
+func _ready() -> void:
+	var type: float = randf()
+	print(type)
+	
+	if type >= 0.95:
+		self.play("flower")
+		print("flower")
+	elif type >= 0.6:
+		self.play("grass2")
--- entities/objects/harvestable.gd
+++ entities/objects/harvestable.gd
@@ -7,14 +7,19 @@ extends Node2D
 @export var respawn_time: float = 0.75 # 0 = never respawn
 
 @onready var base_sprite: Sprite2D = $BaseSprite
+var base_sprite_scale: Vector2
 @onready var harvested_sprite: Sprite2D = $HarvestedSprite
 @onready var text: Sprite2D = $Text
+
+var harvest_sound: AudioStream
+const HARVEST_SOUND = preload("res://audio/creatorshome-sharp-pop-328170.mp3")
 
 var harvested: bool = false
 
 func _ready() -> void: 
 	harvested_sprite.hide()
 	text.hide()
+	base_sprite_scale = base_sprite.scale
 	
 func harvest() -> void:
 	if harvested:
@@ -24,8 +29,17 @@ func harvest() -> void:
 	if debug: print("Harvested ", item.name)
 	
 	harvested = true
+	#base_sprite.hide()
+	#harvested_sprite.show()
+	text.hide()
+	
+	var tween: Tween = create_tween()
+	tween.tween_property(base_sprite, "scale", Vector2.ONE * 1.4, 0.3)\
+		.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+		
+	play_harvest_sound()
+	await tween.finished
 	base_sprite.hide()
-	harvested_sprite.show()
 	
 	if respawn_time > 0:
 		await get_tree().create_timer(respawn_time).timeout
@@ -36,10 +50,11 @@ func respawn() -> void:
 	harvested = false
 	base_sprite.show()
 	harvested_sprite.hide()
+	base_sprite.scale = base_sprite_scale
 
 
 func _on_collectable_radius_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and not harvested:
 		text.show()
 
 
@@ -51,3 +66,11 @@ func _on_collectable_radius_body_exited(body: Node2D) -> void:
 
 func _on_interactable_interacted() -> void:
 	harvest()
+	
+func play_harvest_sound() -> void:
+	var player := AudioStreamPlayer.new()
+	player.stream = HARVEST_SOUND
+	add_child(player)
+	player.play()
+	
+	player.finished.connect(player.queue_free)
